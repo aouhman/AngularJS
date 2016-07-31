e7fd73610e657bbe5109534026c04d48a9ead101
aouhman <abdessamad.ouhman@gmail.com>
2016-07-31 23:13:06 +0100

    Initial commit IIFE

---
+++ script.js
@@ -0,0 +1,28 @@
+// Code JS
+( function () {
+    var workCount = 0;
+
+    var createWorker = function () {
+        var task1 = function () {
+            workCount += 1;
+            console.log("task1 " + workCount);
+        };
+
+        var task2 = function () {
+            workCount += 1;
+            console.log("task2 "  + workCount);
+        };
+        return {
+            job1: task1,
+            job2: task2
+        };
+    };
+
+    var worker = createWorker();
+
+    worker.job1();
+    worker.job2();
+    worker.job2();
+    worker.job2();
+
+}());
